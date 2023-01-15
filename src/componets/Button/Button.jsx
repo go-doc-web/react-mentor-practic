1a4ce223d9aa760e4a9c2230e560908de18ae6b0
Oleg Gubskiy <gubskyolegod@gmail.com>
2023-01-15 22:57:36 +0200

    styled componets , css module

--- src/componets/Button/Button.jsx
+++ src/componets/Button/Button.jsx
@@ -1,13 +1,28 @@
-import './button.css';
+// import css from './button.module.css';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const Wrapper = styled.button`
+  display: inline-block;
+  padding: 6px 12px;
+  background-color: ${({ active }) => (active ? 'red' : '#fff')};
+  color: ${({ active }) => (active ? '#fff' : 'red')};
+  cursor: pointer;
+  border: 1px solid red;
+  border-radius: 6px;
+
+  :hover {
+    background-color: black;
+  }
+`;
 
 const Button = ({ text, type, active }) => {
-  const fullClassName = active ? 'btn active' : 'btn';
+  // const fullClassName = active ? `${css.btn} ${css.active}` : css.btn;
   return (
     // <button className={`btn ${active ? 'active' : ''}`} type={type}>
-    <button className={fullClassName} type={type}>
-      {text}
-    </button>
+    <Wrapper active={active} type={type}>
+      {text}{' '}
+    </Wrapper>
   );
 };
 
